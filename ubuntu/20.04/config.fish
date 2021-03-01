cc7e0eaea6c71cfad132685402cac6b25e804249
Seow Alex <seowalex@hotmail.com>
2021-03-01 23:26:29 +0800

    Use fish_add_path variable

--- ubuntu/20.04/config.fish
+++ ubuntu/20.04/config.fish
@@ -10,15 +10,14 @@ set -gx DISPLAY (grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0
 set -gx LIBGL_ALWAYS_INDIRECT 0
 set -gx LS_COLORS $LS_COLORS'ow=01;34:'
 
-set -gx PATH /usr/local/go/bin $PATH
+fish_add_path /usr/local/go/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path /opt/gradle/gradle-6.6.1/bin
 
-set -gx PATH $HOME/.cargo/bin $PATH
-set -gx PATH /opt/gradle/gradle-6.6.1/bin $PATH
-
-set -gx PATH $HOME/.nodenv/bin $PATH
+fish_add_path $HOME/.nodenv/bin
 status --is-interactive; and nodenv init - | source
 
-set -gx PATH $HOME/.rbenv/bin $PATH
+fish_add_path $HOME/.rbenv/bin
 status --is-interactive; and rbenv init - | source
 
 eval /home/seowalex/miniconda3/bin/conda 'shell.fish' 'hook' $argv | source
