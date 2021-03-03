1d89669085bb967f3cb69e8432b2190f7239d6a6
Seow Alex <seowalex@hotmail.com>
2021-03-03 11:24:47 +0800

    Add cargo to PATH for 18.04

--- ubuntu/18.04/config.fish
+++ ubuntu/18.04/config.fish
@@ -10,6 +10,8 @@ set -gx DISPLAY (grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0
 set -gx LIBGL_ALWAYS_INDIRECT 0
 set -gx LS_COLORS $LS_COLORS'ow=01;34:'
 
+fish_add_path $HOME/.cargo/bin
+
 bass source /usr/share/gazebo-9/setup.sh
 bass source /opt/ros/melodic/setup.bash
 bass source $HOME/bumblebee/ros/devel/setup.bash
