20a318df99dd24b5f5ba72cbe1130cf02acf3f99
Seow Alex <seowalex@hotmail.com>
2021-07-19 01:39:45 +0800

    Fix LS_COLORS

--- ubuntu/18.04/config.fish
+++ ubuntu/18.04/config.fish
@@ -8,7 +8,7 @@ alias open explorer.exe
 
 set -gx DISPLAY (grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0
 set -gx LIBGL_ALWAYS_INDIRECT 0
-set -gx LS_COLORS $LS_COLORS'ow=01;34:'
+set -gx LS_COLORS (string trim -c \' (dircolors -c | string split ' ')[3])'ow=01;34:'
 
 fish_add_path $HOME/.cargo/bin
 
